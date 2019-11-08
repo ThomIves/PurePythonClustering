05ecd6ebe5e3560b6bbb071701c1c68ac4bbec32
ThomIves <thom.ives@gmail.com>
2019-11-07 18:13:55 -0700

    PEP8'd KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
-import random, math, sys, time
+import random
+import math
+import sys
+import time
 
 
 class KMeans(object):
-    def __init__(self, n_clusters=8, init='mn_2sg_rng', n_init=10, 
-        max_iter=300, tol=1e-4):
-        
+    def __init__(self, n_clusters=8, init='mn_2sg_rng', n_init=10,
+                 max_iter=300, tol=1e-4):
+
         self.n_clusters = n_clusters
         self.init = init
         self.n_init = n_init
         self.max_iter = max_iter
         self.tol = tol
 
-        self.clr_arr = ['blue','red','yellow','green','cyan','magenta']
+        self.clr_arr = ['blue', 'red', 'yellow', 'green', 'cyan', 'magenta']
 
     def __get_mins_and_maxs__(self, KNN_A):
         number_of_points = len(KNN_A)
@@ -23,8 +26,8 @@ class KMeans(object):
 
         for i in range(number_of_points):
             for j in range(number_of_dimensions):
-                if KNN_A[i][j] < mins[j]: mins[j] = KNN_A[i][j]
-                if KNN_A[i][j] > maxs[j]: maxs[j] = KNN_A[i][j]
+                mins[j] = KNN_A[i][j] if KNN_A[i][j] < mins[j]
+                maxs[j] = KNN_A[i][j] if KNN_A[i][j] > maxs[j]
 
         return mins, maxs
 
@@ -33,7 +36,7 @@ class KMeans(object):
 
         pt = []
         for i in range(number_of_dimensions):
-            var = random.uniform(-half_range[i],half_range[i])
+            var = random.uniform(-half_range[i], half_range[i])
             pt.append(arr[i]+var)
 
         return pt
@@ -41,29 +44,29 @@ class KMeans(object):
     def __get_random_point_in_range__(self, mins, maxs):
         pt = []
         for i in range(len(mins)):
-            pt.append(random.uniform(mins[i],maxs[i]))
+            pt.append(random.uniform(mins[i], maxs[i]))
 
         return pt
 
     def __get_distribution_parameters_of_pts__(self, KNN_A):
         num_pts = len(KNN_A)
-        number_of_dimensions = len(KNN_A[0]) 
+        number_of_dimensions = len(KNN_A[0])
 
         means = [0] * number_of_dimensions
-        stds  = [0] * number_of_dimensions
+        stds = [0] * number_of_dimensions
 
         # Determine means for each dimension
         for arr in KNN_A:
             for i in range(number_of_dimensions):
                 means[i] += arr[i]
-        for i in range(number_of_dimensions): 
+        for i in range(number_of_dimensions):
             means[i] /= num_pts
 
         # Determine standard deviations for each dimension
         for arr in KNN_A:
             for i in range(number_of_dimensions):
                 stds[i] += (means[i] - arr[i])**2
-        for i in range(number_of_dimensions): 
+        for i in range(number_of_dimensions):
             stds[i] = (stds[i] / num_pts)**0.5
 
         return means, stds
@@ -71,7 +74,7 @@ class KMeans(object):
     def __get_distance_between_two_points__(self, arr1, arr2):
         err_str = 'Error in "__get_distance_between_two_points__":\n'
         err_str += 'Point arrays do not have the same dimensions.'
-        
+
         if len(arr1) != len(arr2):
             raise ArithmeticError(error_string)
 
@@ -104,7 +107,7 @@ class KMeans(object):
         for i in range(len(grps)):
             for j in range(len(grps[i]['points'])):
                 dist = self.__get_distance_between_two_points__(
-                        grps[i]['centroids'], 
+                        grps[i]['centroids'],
                         grps[i]['points'][j])
                 inertia += (dist) ** 2
 
@@ -115,7 +118,7 @@ class KMeans(object):
         total_number_of_clusters = len(grps)
         number_of_dimensions = len(KNN_A[0])
         mins, maxs = self.__get_mins_and_maxs__(KNN_A)
-                
+
         for i in range(total_number_of_clusters):
             number_of_points_in_cluster = len(grps[i]['points'])
             if number_of_points_in_cluster == 0: 
@@ -124,7 +127,7 @@ class KMeans(object):
                     self.__get_random_point_in_range__(mins,maxs))
                 grps[i]['centroids'] = KNN_C_New[-1]
                 continue # then continue
-                
+
             cnt_locs = [0] * number_of_dimensions
 
             for j in range(number_of_dimensions):
@@ -145,13 +148,13 @@ class KMeans(object):
 
         return dist_sum
 
-    def __initial_disbursement_of_centroids__(self, KNN_A, num_cts, 
-        method='mn_2sg_rng'):
+    def __initial_disbursement_of_centroids__(self, KNN_A, num_cts,
+                                              method='mn_2sg_rng'):
         #  method='mean_std_spiral'
         means, stds = self.__get_distribution_parameters_of_pts__(
             KNN_A)
-        
-        two_sig  = []
+
+        two_sig = []
         for element in stds:
             two_sig.append(element*2.0)
 
@@ -161,14 +164,15 @@ class KMeans(object):
             for i in range(num_cts):
                 KNN_C.append(
                     self.__get_random_point_from_point__(
-                        means,two_sig))
+                        means, two_sig))
 
             return KNN_C
 
         elif method == 'mean_std_spiral':
             num_dims = len(means)
             KNN_C.append(means.copy())
-            cnt = 1; radius = 1
+            cnt = 1
+            radius = 1
             while True:
                 for i in range(num_dims):
                     KNN_C.append(means.copy())
@@ -191,7 +195,7 @@ class KMeans(object):
         for attempt in range(self.n_init):
             KNN_C = self.__initial_disbursement_of_centroids__(
                 KNN_A, self.n_clusters)
-            
+
             # Loop beginning
             cnt = 0
             while cnt < self.max_iter:
@@ -201,15 +205,15 @@ class KMeans(object):
 
                 # Find groups by closest to centroid
                 grps = self.__group_points_by_centroids__(grps, KNN_C, KNN_A)
-                
+
                 KNN_C_New, grps = self.__update_centroids__(grps, KNN_A)
 
                 delta_As = self.__find_Arrays_delta__(KNN_C, KNN_C_New)
-            
+
                 if delta_As == 0: break
 
                 KNN_C = KNN_C_New
-                
+
                 cnt += 1
 
             #######
@@ -223,13 +227,15 @@ class KMeans(object):
         return grps_best
 
     def plot_clusters(self, grps):
-        Xc = []; Yc = []
+        Xc = []
+        Yc = []
 
         for i in range(len(grps)):
             Xc.append(grps[i]['centroids'][0])
             Yc.append(grps[i]['centroids'][1])
 
-        X = []; Y = []
+        X = []
+        Y = []
         for i in range(len(grps)):
             X.append([])
             Y.append([])
@@ -260,7 +266,7 @@ class CreateFakeData(object):
 
         pt = []
         for i in range(number_of_dimensions):
-            var = random.uniform(-half_range,half_range)
+            var = random.uniform(-half_range, half_range)
             pt.append(arr[i]+var)
 
         return pt
@@ -268,7 +274,7 @@ class CreateFakeData(object):
     def __get_random_point_in_range__(self, mins, maxs):
         pt = []
         for i in range(len(mins)):
-            pt.append(random.uniform(mins[i],maxs[i]))
+            pt.append(random.uniform(mins[i], maxs[i]))
 
         return pt
 
@@ -277,30 +283,33 @@ class CreateFakeData(object):
         KNN_A = []
 
         for seed in self.seeds:
-            for i in range(self.points_per_cluster): 
+            for i in range(self.points_per_cluster):
                 KNN_A.append(self.__get_random_point_from_point__(
                     seed, self.half_range))
 
         return KNN_A
 
+
 ###############################################################################
 def scatter_plot_points(pts):
-    X = []; Y = []
+    X = []
+    Y = []
 
     for i in range(len(pts)):
         X.append(pts[i][0])
         Y.append(pts[i][1])
-        
+
     plt.scatter(X, Y)
     plt.xlabel('X Vals')
     plt.ylabel('Y Vals')
     plt.title('Fake Data Points')
     plt.show()
 
+
 ###############################################################################
 # Setup Data
-clr_arr = ['blue','red','yellow','green','cyan','magenta']
-seeds = [[3,10], [10,3], [3,3], [10,10], [17,6]]
+clr_arr = ['blue', 'red', 'yellow', 'green', 'cyan', 'magenta']
+seeds = [[3, 10], [10, 3], [3, 3], [10, 10], [17, 6]]
 half_range = 2
 
 # Create Fake Data
@@ -308,12 +317,10 @@ fake_data = CreateFakeData(seeds)
 KNN_A = fake_data.create_fake_data()
 scatter_plot_points(KNN_A)
 
-# Find the Clusters 
+# Find the Clusters
 kmeans = KMeans(n_clusters=5)
 grps = kmeans.determine_k_clusters(KNN_A)
 print('Inertia is {}.'.format(kmeans.inertia_))
 kmeans.plot_clusters(grps)
 
 ###############################################################################
-
-
